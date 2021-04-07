35b770f1c8c75da7aa381dd30c69dcd56c59c116
Knut Maurset <knut.espen.maurset@dnb.no>
2021-04-07 13:29:14 +0200

    jobber i react

--- assignment2-react/src/App.tsx
+++ assignment2-react/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Menu from './Menu';
-import ViewConfigData from './ViewConfigData';
+import ViewConfigData from './viewConfigData/ViewConfigData';
 
 
 export default function App() {
